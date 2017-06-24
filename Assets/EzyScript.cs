b884d68ec9f1bb4c010f94ec9ac0783cd91ed699
Ezydenias <kleinermaximilian@googlemail.com>
2017-06-24 13:28:33 +0200

    Implemented Animation Script and Improved Animator

---
+++ Assets/EzyScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EzyScript : MonoBehaviour
+{
+    Animator animator;
+    int jumpHash = Animator.StringToHash("Jump");
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float move = Input.GetAxis("Vertical");
+	    animator.SetFloat("Speed", move);
+
+	    if (Input.GetKeyDown(KeyCode.Space))
+	    {
+	        animator.SetTrigger(jumpHash);
+	    }
+	}
+}
