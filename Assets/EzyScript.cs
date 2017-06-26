9f18d0ef94cde98bf67c4853ab1b0febae924a67
Ezydenias <kleinermaximilian@googlemail.com>
2017-06-26 15:54:09 +0200

    Implemented end of Jump Implemented that the cara will play the standing animation once it has hit the ground, still has the bug that it doesn't properly transforms the jump animation if object is to close to the ground at the beginning of it's upwards movement.

--- Assets/EzyScript.cs
+++ Assets/EzyScript.cs
@@ -6,23 +6,55 @@ public class EzyScript : MonoBehaviour
 {
     Animator animator;
     int jumpHash = Animator.StringToHash("Jump");
+    int doubleJumpHash = Animator.StringToHash("DoubleJump");
+    int groundedHash = Animator.StringToHash("GroundedTotally");
+    private bool Jumped = false;        //checks if character jumps for doublejump animation
+    public float distToGrounded = 15.0f;
+    public LayerMask ground;
+    Transform tr;
 
 
-	// Use this for initialization
-	void Start ()
-	{
-	    animator = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	    float move = Input.GetAxis("Vertical");
-	    animator.SetFloat("Speed", move);
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        tr = transform;
+    }
 
-	    if (Input.GetKeyDown(KeyCode.Space))
-	    {
-	        animator.SetTrigger(jumpHash);
-	    }
-	}
+    //checks if character is grounded
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGrounded);
+    }
+
+    void LateUpdate()
+    {
+        if ((IsGrounded()) && Jumped)
+        {
+            animator.SetTrigger(groundedHash);
+            Jumped = false;
+        }
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Vertical");
+        animator.SetFloat("Speed", move);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Jumped == false)        //if character doesn't jump, it jumps
+            {
+                animator.SetTrigger(jumpHash);
+                animator.SetBool("Grounded", false);
+                Jumped = true;
+            }
+            else                        //if character jumped, performed the doublejump animation
+            {  
+                animator.SetTrigger(doubleJumpHash);
+                //Jumped = false;
+            }
+        }
+    }
 }
